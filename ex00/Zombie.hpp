acd66d487220c76025de02b207d1090703a99307
Badr eddine Souhar <bsouhar@c1r3p1.1337.ma>
2023-08-25 22:37:10 +0100

    cpp

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsouhar <bsouhar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 23:45:18 by bsouhar           #+#    #+#             */
+/*   Updated: 2023/08/24 23:49:19 by bsouhar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+#include <string>
+
+class Zombie
+{
+private:
+    std::string name;
+public:
+    Zombie(/* args */);
+    ~Zombie();
+};
+
+Zombie::Zombie(/* args */)
+{
+}
+
+Zombie::~Zombie()
+{
+}
+
+#endif
